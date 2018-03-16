b59f15fd34cebfcbfb621f77cb635bbe56166f36
paulyao <peter1992620@gmail.com>
2018-03-16 20:16:07 +0800

    add

---
+++ Assets/Scripts/particle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.position.x>0.56f)
+        transform.Translate(new Vector3(-0.007f, 0, 0));
+	}
+}
